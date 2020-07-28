34f6d64b4afffcb82a2fb46c4e1f86415c611fad
Sage Lee <sagemcklee@gmail.com>
2020-07-28 10:55:50 -0600

    Binary tree depth_of method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,22 +4,28 @@ class BinarySearchTree
         @total = Hash.new
     end 
     def insert(score, title)
-        @total[score] = title
-        @total.count/2 
+        depth = [title]
+        @total[score] = depth
+        depth << @total.count/2 
     end 
 
     def include?(score)
         @total.has_key?(score)
     end 
+
+    def depth_of(score)
+      @total[score][1]
+    end 
 end 
 
 tree = BinarySearchTree.new
-
 tree.insert(61, "Bill & Ted's Excellent Adventure")
 tree.insert(16, "Johnny English")
 tree.insert(92, "Sharknado 3")
 tree.insert(50, "Hannibal Buress: Animal Furnace")
-        binding.pry
 
 tree.include?(16)
 tree.include?(72)
+
+tree.depth_of(92)
+tree.depth_of(50)
